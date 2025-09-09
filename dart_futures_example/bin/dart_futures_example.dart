18b005c0585b950cc8295e925f8a27aeaaf648b8
Youfang yao <yyfsingler@163.com>
2025-09-09 10:47:50 -0600

    add function getJson()

--- dart_futures_example/bin/dart_futures_example.dart
+++ dart_futures_example/bin/dart_futures_example.dart
@@ -1,5 +1,13 @@
-import 'package:dart_futures_example/dart_futures_example.dart' as dart_futures_example;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
   
+}
+
+Future<dynamic> getJson(String endpoint) async {
+  var uri = Uri.parse(endpoint);  // Create the URI Object from the string
+  var response = await http.get(uri); // Make the http request
+
+  return jsonDecode(response.body);
 }
