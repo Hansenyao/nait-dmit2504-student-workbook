fc0a5d71716e0620d488ea6dd00d17fd72b164de
Youfang yao <yyfsingler@163.com>
2025-09-09 10:38:25 -0600

    add the initail versio

---
+++ dart_futures_example/bin/dart_futures_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_futures_example/dart_futures_example.dart' as dart_futures_example;
+
+void main(List<String> arguments) {
+  
+}
---
+++ dart_futures_example/lib/models/Dog.dart
@@ -0,0 +1,9 @@
+class Dog {
+  late Uri imageUri;
+  late String status;
+
+  Dog({required String endpoint, 
+      required this.status}) {
+        imageUri = Uri.parse(endpoint);
+  }
+}
