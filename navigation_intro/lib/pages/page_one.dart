6e4a65872fe7947a518282b6d496eb09f08521a9
Youfang yao <yyfsingler@163.com>
2025-10-01 09:35:52 -0600

    return string

--- navigation_intro/lib/pages/page_one.dart
+++ navigation_intro/lib/pages/page_one.dart
@@ -10,6 +10,8 @@ class PageOne extends StatefulWidget {
 }
 
 class _PageOneState extends State<PageOne> {
+  var data = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +21,17 @@ class _PageOneState extends State<PageOne> {
           children: [
             Text('Page One'),
             ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(routes.pageTwoRoute);
+              onPressed: () async {
+                var result = await Navigator.of(
+                  context,
+                ).pushNamed(routes.pageTwoRoute);
+                setState(() {
+                  data = result as String;
+                });
               },
               child: Text('Route to Page Two'),
             ),
+            data.isEmpty ? Text('Awating Data') : Text('Recieved: $data'),
           ],
         ),
       ),
