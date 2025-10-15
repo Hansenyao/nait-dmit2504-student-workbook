87ea76b440e7a469ec100229b52ab6db28d23a49
Youfang yao <yyfsingler@163.com>
2025-10-15 08:52:40 -0600

    add pages for firebase_dmit_2540_f2025

--- firebase_dmit_2504_f2025/lib/main.dart
+++ firebase_dmit_2504_f2025/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_dmit_2504_f2025/pages/app_state.dart';
+import 'package:firebase_dmit_2504_f2025/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -5,6 +7,9 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final appState = ApplicationState();
+
   runApp(const MainApp());
 }
 
@@ -13,8 +18,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    final routes = {
+      '/': (context) {
+        return HomePage();
+      },
+    };
+    return MaterialApp(routes: routes);
   }
 }
---
+++ firebase_dmit_2504_f2025/lib/pages/app_state.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_dmit_2504_f2025/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+
+  bool _loggedIn = false;
+  bool get isLongIn => _loggedIn;
+
+  void init() async {
+    // Connect to firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // set up our auth providers
+    FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
+
+    // Bind a listener
+    FirebaseAuth.instance.userChanges().listen((user) {
+      // when the user changes check if null and update loggedIn accordingly
+      if (user != null) {
+        _loggedIn = true;
+      } else {
+        _loggedIn = false;
+      }
+    });
+  }
+}
