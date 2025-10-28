eeaadbf7523b2f33b9b30d338c5bf8bf4d66f036
Youfang yao <yyfsingler@163.com>
2025-10-28 11:41:57 -0600

    finished last  name page for bloc_state

--- bloc_state/lib/pages/first_name_page.dart
+++ bloc_state/lib/pages/first_name_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bloc_state/models/user.dart';
 import 'package:bloc_state/state/user_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
