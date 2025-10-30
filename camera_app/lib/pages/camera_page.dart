fee20081de4c725a168320db7593c568867ef418
Youfang yao <yyfsingler@163.com>
2025-10-30 10:56:29 -0600

    finish take picture feature

--- camera_app/lib/pages/camera_page.dart
+++ camera_app/lib/pages/camera_page.dart
@@ -10,12 +10,61 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      widget.cameraDescription,
+      ResolutionPreset.max,
+    );
+
+    _controller
+        .initialize()
+        .then((_) {
+          // Once the controller is initialized, it's good practice to rerender the screen to avoid any wonkiness
+          setState(() {});
+        })
+        .catchError((e) {
+          // Here is where you handle errors with the camera permission denied
+          if (e is CameraException) {
+            if (e.code == "CamerAccessDenied") {}
+          }
+          // Handle other exceptions here
+        });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<XFile?> takePicture() async {
+    // First we make sure we're not currently in the process of taking a picture
+    if (_controller.value.isTakingPicture) return null;
+
+    try {
+      var file = await _controller.takePicture();
+      return file;
+    } catch (e) {
+      // If something goes wrong you can decide how to handle that.
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Camera Page')),
       body: Center(
-        child: Column(children: [Text("Welcome to the camera page!")]),
+        child: Column(children: [Expanded(child: CameraPreview(_controller))]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.camera),
       ),
     );
   }
